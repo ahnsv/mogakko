3c046943e44d2dfd6e0fce84e75e55d1ab7735ce
Humphrey Ahn <ahnsv@bc.edu>
2019-05-12 20:57:48 +0900

    mogakko init

---
+++ mogakko.py
@@ -0,0 +1,36 @@
+payload = {
+    'assignee': '',
+    'task': [
+        {'name': '',
+         'due': '',
+         'status': ''},
+    ]
+}
+
+daily_task = []
+
+
+def mogakko_add(data):
+    """
+
+    :param data: [{assignee: string, task: string list}]
+    :return:
+    """
+    # Get today's to-do list
+    # Update the list
+    # Send updated list to client
+
+
+def mogakko_update(data):
+    """
+    Update mogakko to-do with post request
+    :param data:
+    :return:
+    """
+
+def mogakko_delete(data):
+    """
+    Delete mogakko to-do
+    :param data:
+    :return:
+    """
