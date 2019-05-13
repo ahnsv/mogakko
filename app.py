fb4b9a1e3740b7eb5eef311288ab8a72f54bd3f9
Humphrey Ahn <ahnsv@bc.edu>
2019-05-13 22:46:58 +0900

    feat: mongodb connection success

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/local"
+mongo = PyMongo(app)
 
 
 @app.route("/", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def mogakko():
+    mongo.db.mogakko.insert_one({'data': 'hello'})
+    # TODO: handle HTTP requests to CRUD data from frontend
     return "HELLO"
 
 
--- mogakko.py
+++ mogakko.py
@@ -1,36 +1,47 @@
-payload = {
-    'assignee': '',
-    'task': [
-        {'name': '',
-         'due': '',
-         'status': ''},
-    ]
-}
+class mogakko(object):
+    def __init__(self):
+        super(mogakko, self).__init__()
+        self.payload = {
+            'assignee': '',
+            'task': [
+                {'name': '',
+                 'due': '',
+                 'status': ''},
+            ],
+            'date': 'YYYY-MM-DD',
+            'id': 1
+        }
+        self.daily_task = []
 
-daily_task = []
+    def list(self, query):
+        """
+        List all to-do lists queried by either date, id, or assignee
+        :param query:
+        :return:
+        """
+        return sorted(self.daily_task, key=query)
 
+    def create(self, data):
+        # Get today's to-do list
+        self.daily_task.append(data)
 
-def mogakko_add(data):
-    """
+    def update(self, payload):
+        """
+        Update mogakko to-do with post request
+        :param payload:
+        :return:
+        """
+        try:
+            list(filter(lambda x: x.id == payload.id, self.daily_task))[0] = payload
+        except ValueError:
+            print('nothing found')
 
-    :param data: [{assignee: string, task: string list}]
-    :return:
-    """
-    # Get today's to-do list
-    # Update the list
-    # Send updated list to client
+    def findListById(self, id):
+        return list(filter(lambda x: x.id == id, self.daily_task))[0]
 
-
-def mogakko_update(data):
-    """
-    Update mogakko to-do with post request
-    :param data:
-    :return:
-    """
-
-def mogakko_delete(data):
-    """
-    Delete mogakko to-do
-    :param data:
-    :return:
-    """
+    def delete(self, id):
+        """
+        Delete mogakko to-do by id
+        :param id:
+        :void:
+        """
